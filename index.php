b6c9c69c3329d6a977f847d18b1372fe038ca15c
Gbike <59223522+Gbike@users.noreply.github.com>
2020-02-15 13:56:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ $stickerId = 34; //stickerId
 
 	$sToken = "WxNhlhiKwsJLUz5LOess1O7cMyG0yuW125XpBNfTW0u";
 	$sMessage = "มีรายการสั่งซื้อเข้าเด้อ....";
+        
 
 	
 	$chOne = curl_init(); 
@@ -22,6 +23,82 @@ $stickerId = 34; //stickerId
 	curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
 	curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
 	$result = curl_exec( $chOne ); 
+
+$jsonFlex = [
+    "type" => "flex",
+    "altText" => "เวลาเข้า-ออกงาน",
+    "contents" => [
+      "type" => "bubble",
+//      "size" => "giga",
+      "direction" => "ltr",
+      "header" => [
+        "type" => "box",
+        "layout" => "vertical",
+        "contents" => [
+          [
+            "type" => "text",
+            "text" => "บันทึกเวลา",
+            "size" => "lg",
+            "align" => "start",
+            "weight" => "bold",
+            "color" => "#009813",
+          ],
+          [
+            "type" => "separator",
+            "margin" => "lg",
+            "color" => "#C3C3C3"
+          ],
+          [
+            "type" => "text",
+            "text" => "$new_date",
+            "align" => "center",                   
+            "size" => "3xl",
+            "weight" => "bold",
+            "color" => "#000000"
+          ],
+          [
+            "type" => "text",
+            "text" => "$new_time",
+            "align" => "center",              
+            "size" => "4xl",
+            "weight" => "bold",
+            "color" => "#0000ff"
+          ],
+//          [
+//            "type" => "text",
+//            "text" => "สาขาแว่นแก้ว",
+//            "size" => "lg",
+//            "weight" => "bold",
+//            "color" => "#000000"
+//          ],
+          [
+            "type" => "separator",
+            "margin" => "lg",
+            "color" => "#C3C3C3"
+          ]
+        ]
+      ],
+      "footer" => [
+        "type" => "box",
+        "layout" => "horizontal",
+        "contents" => [
+          [
+            "type" => "text",
+            "text" => "รายละเอียด",
+            "size" => "lg",
+            "align" => "start",
+            "color" => "#0084B6",
+            "action" => [
+              "type" => "uri",
+              "label" => "รายละเอียด",
+              "uri" => "https://www.google.co.th/"
+            ]
+          ]
+        ]
+      ]
+    ]
+  ];
+
 
 	//Result error 
 	if(curl_error($chOne)) 
